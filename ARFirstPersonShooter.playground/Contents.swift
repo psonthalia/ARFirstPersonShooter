c3cd15297f9b9cd1df8db26949e73115c9d44039
Paran Sonthalia <paran.sonthalia@gmail.com>
2019-03-19 21:34:02 -0700

    ability to add as many soldiers as needed

--- ARFirstPersonShooter.playground/Contents.swift
+++ ARFirstPersonShooter.playground/Contents.swift
@@ -7,11 +7,11 @@ import ARKit
 class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     let session = ARSession()
     var sceneView: ARSCNView!
-    var hasPlacedSoldier = false
-    var soldier: SCNNode?
-    var characterNode: SCNNode?
-    var characterOrientation: SCNNode?
     var cameraNode = SCNNode()
+    
+    var numberOfSoldiers = 5;
+    var soldiersPlacedCount = 0;
+    var soldierArray: [SCNNode?] = []
 
     override func loadView() {
         sceneView = ARSCNView(frame: CGRect(x: 0.0, y: 0.0, width: 500.0, height: 600.0))
@@ -22,65 +22,32 @@ class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDe
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = .horizontal
 
-        // set up scene view
         sceneView.setup()
-        // Set the view's delegate
         sceneView.delegate = self
         sceneView.session = session
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
-                                  ARSCNDebugOptions.showWorldOrigin/*,
-                                  .showBoundingBoxes,
-                                  .showWireframe,
-                                  .showSkeletons,
-                                  .showPhysicsShapes,
-                                  .showCameras*/
-                                ]
-        
         sceneView.showsStatistics = true
-        
-        // Now we'll get messages when planes were detected...
         sceneView.session.delegate = self
-
-        // default lighting
         sceneView.autoenablesDefaultLighting = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // a camera
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
         scene.rootNode.addChildNode(cameraNode)
         
-        soldier = soldierNode()
-        characterNode = SCNNode()
-        characterNode!.name = "character"
-        characterNode!.simdPosition = float3(0.1, -0.2, 0)
-        
-        characterOrientation = SCNNode()
-        characterNode!.addChildNode(characterOrientation!)
-        characterOrientation!.addChildNode(soldier!)
-
         
         self.view = sceneView
         sceneView.session.run(config, options: [.resetTracking, .removeExistingAnchors])
     }
     
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        print(self.cameraNode.rotation)
         for anchor in anchors {
-            if ( !hasPlacedSoldier ) {
-                soldier?.position = SCNVector3(anchor.transform.columns.3.x, anchor.transform.columns.3.y, anchor.transform.columns.3.z)
-                sceneView.scene.rootNode.addChildNode(soldier!)
-                DispatchQueue.main.async {
-                    self.hasPlacedSoldier = true
-                }
-                soldier?.animationPlayer(forKey: "idle")?.play()
-            } else {
-                let friend = soldierNode()
-                friend.position = SCNVector3(anchor.transform.columns.3.x, anchor.transform.columns.3.y, anchor.transform.columns.3.z)
-                guard let geometryNode = friend.childNode(withName: "soldier", recursively: true) else { return }
+            if (soldiersPlacedCount < numberOfSoldiers) {
+                let soldier = soldierNode()
+                soldier.position = SCNVector3(anchor.transform.columns.3.x, anchor.transform.columns.3.y, anchor.transform.columns.3.z)
                 
-                geometryNode.geometry!.firstMaterial?.diffuse.intensity = 0.5
-                
-                sceneView.scene.rootNode.addChildNode(friend)
+                soldierArray.append(soldier)
+                soldiersPlacedCount += 1
+                sceneView.scene.rootNode.addChildNode(soldier)
             }
         }
     }
@@ -105,21 +72,12 @@ class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDe
 
 
 extension ARSCNView {
-    
     func setup() {
         antialiasingMode = .multisampling4X
         automaticallyUpdatesLighting = false
         
         preferredFramesPerSecond = 60
         contentScaleFactor = 1.3
-        
-//        if let camera = pointOfView?.camera {
-//            camera.wantsHDR = true
-//            camera.wantsExposureAdaptation = true
-//            camera.exposureOffset = -1
-//            camera.minimumExposure = -1
-//            camera.maximumExposure = 3
-//        }
     }
 }
 
