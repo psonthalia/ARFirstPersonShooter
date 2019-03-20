63ae3080de4946021e7e3f593ed83245478b72e9
Paran Sonthalia <paran.sonthalia@gmail.com>
2019-03-20 01:53:41 -0700

    got tap on head to kill enemy

--- ARFirstPersonShooter.playground/Contents.swift
+++ ARFirstPersonShooter.playground/Contents.swift
@@ -9,9 +9,9 @@ class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDe
     var sceneView: ARSCNView!
     var cameraNode = SCNNode()
     
-    var numberOfSoldiers = 5;
+    var numberOfSoldiers = 2;
     var soldiersPlacedCount = 0;
-    var soldierArray: [SCNNode?] = []
+    var soldierSet = Set<SCNNode>()
 
     override func loadView() {
         sceneView = ARSCNView(frame: CGRect(x: 0.0, y: 0.0, width: 500.0, height: 600.0))
@@ -34,6 +34,8 @@ class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDe
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
         scene.rootNode.addChildNode(cameraNode)
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(rec:)))
+        sceneView.addGestureRecognizer(tap)
         
         self.view = sceneView
         sceneView.session.run(config, options: [.resetTracking, .removeExistingAnchors])
@@ -42,16 +44,42 @@ class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDe
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         for anchor in anchors {
             if (soldiersPlacedCount < numberOfSoldiers) {
-                let soldier = soldierNode()
+                let soldier = soldierNode("soldier " + String(soldiersPlacedCount))
                 soldier.position = SCNVector3(anchor.transform.columns.3.x, anchor.transform.columns.3.y, anchor.transform.columns.3.z)
+                self.sceneView.scene.rootNode.addChildNode(soldier)
                 
-                soldierArray.append(soldier)
-                soldiersPlacedCount += 1
-                sceneView.scene.rootNode.addChildNode(soldier)
+                self.soldierSet.insert(soldier)
+                self.soldiersPlacedCount += 1
             }
         }
     }
-    // An exercise for the reader: make the characters respond to the orientation of the camera and turn accordingly.
+
+    @objc func handleTap(rec: UITapGestureRecognizer) {
+        if soldiersPlacedCount == numberOfSoldiers {
+            let location: CGPoint = rec.location(in: sceneView)
+            let hits = sceneView.hitTest(location, options: nil)
+            if !hits.isEmpty {
+                for hit in hits {
+                    let tappedNode = hit.node
+                    if (soldierSet.contains(tappedNode)) {
+                        soldierSet.remove(tappedNode)
+                        tappedNode.removeFromParentNode()
+                    }
+                }
+                
+            }
+        }
+    }
+
+    func soldierNode(_ name: String) -> SCNNode {
+        let soldierScene = SCNScene(named: "Art.scnassets/character/soldier.scn")!
+        let soldier = soldierScene.rootNode.childNode(withName: "soldier", recursively: true)!
+        soldier.name = name
+        soldier.scale = SCNVector3(x: 0.03, y: 0.03, z: 0.03)
+        return soldier
+    }
+    
+    
 //    public func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
 //        DispatchQueue.main.async {
 //            // Rotate Max to face the camera?
@@ -60,14 +88,6 @@ class QISceneKitViewController: UIViewController, ARSCNViewDelegate, ARSessionDe
 //                SCNAction.rotateTo(x: self.cameraNode.frame.maxX, y: self.cameraNode.frame.maxY, z: 0.0, duration: 0.1, usesShortestUnitArc:true))
 //        }
 //    }
-
-
-    func soldierNode() -> SCNNode {
-        let soldierScene = SCNScene(named: "Art.scnassets/character/soldier.scn")!
-        let soldier = soldierScene.rootNode.childNode(withName: "soldier", recursively: true)!
-        soldier.scale = SCNVector3(x: 0.03, y: 0.03, z: 0.03)
-        return soldier
-    }
 }
 
 
